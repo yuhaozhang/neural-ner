9627d967f92292ee05535ac3e919b9220cc3e85b
Yuhao Zhang <zyh@stanford.edu>
2019-02-07 17:29:01 -0800

    Update training scripts

--- train_cnn_lstm_crf.sh
+++ train_cnn_lstm_crf.sh
@@ -8,8 +8,8 @@ fi
 ID=$1; shift
 ARGS=$@
 python train.py --id $ID --data_dir dataset/conll_iobes --scheme iobes \
-    --vocab_dir dataset/vocab_all_100d --emb_dim 100 --hidden_dim 200 --num_layers 1 --lower --crf \
+    --vocab_dir dataset/vocab --emb_dim 100 --hidden_dim 200 --num_layers 1 --lower --crf \
     --char_type cnn --char_emb_dim 30 --char_fmin 3 --char_fmax 3 --char_fsize 30 --char_cnn_dim -1 \
-    --optim sgd --lr 0.015 --lr_decay 0.9 --momentum 0.9 --batch_size 10 --num_epoch 150 --decay_epoch 100 \
+    --optim sgd --lr 0.015 --lr_decay 0.9 --momentum 0.9 --batch_size 10 --num_epoch 100 --decay_epoch 50 \
     --info "Train a CNN-LSTM-CRF NER model" $ARGS
 
--- train_lstm_crf.sh
+++ train_lstm_crf.sh
@@ -8,8 +8,8 @@ fi
 ID=$1; shift
 ARGS=$@
 python train.py --id $ID --data_dir dataset/conll_iobes --scheme iobes\
-    --vocab_dir dataset/vocab_all_100d --emb_dim 100 --hidden_dim 100 --num_layers 1 --lower --crf \
+    --vocab_dir dataset/vocab --emb_dim 100 --hidden_dim 100 --num_layers 1 --lower --crf \
     --char_type rnn --char_emb_dim 25 --char_hidden_dim 25 --dropout 0.5 --word_dropout 0.0\
-    --optim sgd --lr 0.1 --momentum 0.9 --batch_size 10 --num_epoch 120 --decay_epoch 80\
-    --info "A BiLSTM-CRF NER model" $ARGS
+    --optim sgd --lr 0.1 --lr_decay 0.9 --momentum 0.9 --batch_size 10 --num_epoch 100 --decay_epoch 50\
+    --info "An LSTM-CRF NER model" $ARGS
 
